ee44dfc8de6afbd1a038a4d81c890c6ad52247f3
Mariha Kozinska <m.kozinska@gmail.com>
2020-07-09 10:22:42 -0500

    Logging

--- src/main/java/pl/wanderers/footprint/resources/HelloWorldResource.java
+++ src/main/java/pl/wanderers/footprint/resources/HelloWorldResource.java
@@ -1,14 +1,20 @@
 package pl.wanderers.footprint.resources;
 
+import com.codahale.metrics.annotation.Metered;
 import com.codahale.metrics.annotation.Timed;
+import io.dropwizard.jersey.caching.CacheControl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import pl.wanderers.footprint.api.Greetings;
@@ -17,6 +23,9 @@ import pl.wanderers.footprint.core.Template;
 @Path("/hello-world")
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldResource.class);
+
     private final Template template;
     private final AtomicLong counter;
 
@@ -26,8 +35,16 @@ public class HelloWorldResource {
     }
 
     @GET
-    @Timed
+    @Timed(name = "get-requests-timed")
+    @Metered(name = "get-requests-metered")
+    @CacheControl(maxAge = 1, maxAgeUnit = TimeUnit.DAYS)
     public Greetings sayHello(@QueryParam("name") @Nullable String name) {
+        logger.info("Requested to say hello: {}", name);
         return new Greetings(counter.incrementAndGet(), template.render(Optional.ofNullable(name)));
+    }
+
+    @POST
+    public void receiveHello(Greetings saying) {
+        logger.info("Received a saying: {}", saying);
     }
 }
